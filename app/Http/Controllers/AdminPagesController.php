9406875220b45d9f2bc79896a335cb56cb841846
James McHugh <james.mchugh1988@gmail.com>
2019-10-12 14:51:27 +0100

    created the routes

--- app/Http/Controllers/AdminPagesController.php
+++ app/Http/Controllers/AdminPagesController.php
@@ -6,11 +6,32 @@ use Illuminate\Http\Request;
 
 class AdminPagesController extends Controller
 {
-    public function __construct(){
+      /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+
+    protected $redirectTo = '/admin/login';
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->middleware('auth');
 
     }
-    
-    public function index(){
 
+    protected function guard()
+    {
+        return Auth::guard('admin');
+    }
+
+
+    public function index(){
+      $data = array(
+
+      );
+
+      return view("backend.pages.dash")->with($data);
     }
 }
