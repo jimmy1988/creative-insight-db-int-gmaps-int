5dcbc9656ae1565698b0d7553f05182189aae8d1
James McHugh <james.mchugh1988@gmail.com>
2019-10-12 14:50:38 +0100

    ammended the redirect route

--- app/Http/Controllers/Auth/Backend/BackendLoginController.php
+++ app/Http/Controllers/Auth/Backend/BackendLoginController.php
@@ -27,7 +27,7 @@ class BackendLoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/';
+    protected $redirectTo = '/admin/login';
 
     /**
      * Create a new controller instance.
