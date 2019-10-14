3f2325795615c89e44f9d849203131904421de91
James McHugh <james.mchugh1988@gmail.com>
2019-10-14 22:27:25 +0100

    new functions in the main controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\AdminUsers;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Support\Facades\Auth;
 
 class Controller extends BaseController{
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
@@ -14,5 +16,33 @@ class Controller extends BaseController{
 
     public function __construct(){
 
+    }
+
+    protected static function currentUserDetails(){
+      return Auth::guard('admin')->user()->toArray();
+    }
+
+    protected static function userIsAllowed($useridhash = null, $userLevel = 7){
+      //if user id is not set user the current user
+      if(isset($useridhash) && !empty($useridhash)){
+        $userid = Auth::guard('admin')->user()->user_id_hash;
+      }
+
+      //if user level is less than 1 then set it to 8 - which is the basic user - this is a fallback for incorrect values
+      if(isset($userLevel) && !empty($userLevel) && $userid <= 0){
+        $userLevel = 8;
+      }
+
+      $user = AdminUsers::where("user_id_hash", $useridhash)->first();
+
+      if(isset($user) && !empty($user) && $user->count() > 0){
+        if(isset($user->user_level) && !empty($user->user_level) && $user->user_level <= $userLevel){
+          return true;
+        }else{
+          return false;
+        }
+      }else{
+        return false;
+      }
     }
 }
