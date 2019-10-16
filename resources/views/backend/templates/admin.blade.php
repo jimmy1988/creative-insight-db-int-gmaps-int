ed3ee7d69504cc58bd40ef0754ca8ccb3bf990f7
James McHugh <james.mchugh1988@gmail.com>
2019-10-16 17:45:20 +0100

    removed title bar and messages from the admin template

--- resources/views/backend/templates/admin.blade.php
+++ resources/views/backend/templates/admin.blade.php
@@ -8,8 +8,7 @@
 
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
-  @include('backend.layouts.titlebar')
-  @include('backend.includes.template-messages')
+  
   <div class="main-content">
         @yield('content')
   </div>
