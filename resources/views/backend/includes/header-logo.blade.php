78fa225d8b9258dcfef8a63f6e1210d3ead217ed
James McHugh <james.mchugh1988@gmail.com>
2019-10-16 14:34:31 +0100

    logo construction

--- resources/views/backend/includes/header-logo.blade.php
+++ resources/views/backend/includes/header-logo.blade.php
@@ -1,7 +1,11 @@
 <!-- Logo -->
-<a href="#" class="logo">
+<a href="/" class="logo">
   <!-- mini logo for sidebar mini 50x50 pixels -->
-  <span class="logo-mini"><b>A</b>LT</span>
+  <span class="logo-mini">
+    <img alt="Project Logo" src="/images/logo.png" class="main-nav-logo-admin"/>
+  </span>
   <!-- logo for regular state and mobile devices -->
-  <span class="logo-lg"><b>Admin</b>LTE</span>
+  <span class="logo-lg">
+    <img alt="Project Logo" src="/images/logo.png" class="main-nav-logo-admin"/>
+  </span>
 </a>
