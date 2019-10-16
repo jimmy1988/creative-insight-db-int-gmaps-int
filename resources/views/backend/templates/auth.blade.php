9d12270ab406de7fa9d2cf1459f49df502611429
James McHugh <james.mchugh1988@gmail.com>
2019-10-16 14:23:49 +0100

    added new styles and classes

--- resources/views/backend/templates/auth.blade.php
+++ resources/views/backend/templates/auth.blade.php
@@ -2,9 +2,15 @@
 
 @include('backend.layouts.top')
 
+<style type="text/css">
+  .logo-mini .main-nav-logo-admin{
+    display: none;
+  }
+</style>
+
 @include('backend.layouts.auth-header')
 
-<div class="main-content">
+<div class="main-content auth-content">
   @yield('content')
 </div>
 
