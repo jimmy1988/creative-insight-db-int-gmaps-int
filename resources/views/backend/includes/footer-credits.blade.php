f28b3fd4832164d4ed1a963c963d507ea173ee7b
James McHugh <james.mchugh1988@gmail.com>
2019-10-17 14:37:24 +0100

    new footer creds from the env file

--- resources/views/backend/includes/footer-credits.blade.php
+++ resources/views/backend/includes/footer-credits.blade.php
@@ -1,5 +1,5 @@
 <div class="pull-right hidden-xs">
-  <b>Version</b> 2.4.0
+  <b>Version</b> {{env('APP_VERSION')}}
 </div>
-<strong>Copyright &copy; 2014-2016 <a href="#">Almsaeed Studio</a>.</strong> All rights
+<strong>Copyright &copy; 2014-2016. {{env('APP_NAME')}},  <a href="#">{{ env('APP_AUTHOR') }}</a>.</strong> All rights
 reserved.
