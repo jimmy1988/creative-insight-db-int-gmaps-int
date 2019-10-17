fc5c20d5e7d3c7ee2ea296aa228c9de6ec5434bc
James McHugh <james.mchugh1988@gmail.com>
2019-10-17 16:46:58 +0100

    completed sidebar

--- resources/views/backend/layouts/sidebar.blade.php
+++ resources/views/backend/layouts/sidebar.blade.php
@@ -5,18 +5,42 @@
 
     <!-- sidebar menu: : style can be found in sidebar.less -->
     <ul class="sidebar-menu" data-widget="tree">
-      {{-- <li class="treeview">
+      <li>
+        <a href="{{route('admin.index')}}" class="btn btn-info text-black text-left">
+          <i class="fas fa-tachometer-alt"></i>&nbsp;<span>Dashboard</span>
+        </a>
+      </li>
+      <li>
+        <a href="{{route('index')}}" target="_blank" class="btn btn-success text-black text-left">
+          <i class="fas fa-globe-europe"></i>&nbsp;<span>Website</span>
+        </a>
+      </li>
+      <li class="treeview">
         <a href="#">
-          <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+          <i class="fas fa-location-arrow"></i>&nbsp;<span>Places</span>
           <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
           </span>
         </a>
         <ul class="treeview-menu">
-          <li><a href="/template/index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-          <li><a href="/template/index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
+          <li><a href="{{route('admin.places')}}"><i class="fas fa-map-marker-alt"></i>&nbsp;All Places</a></li>
+          <li><a href="{{route('admin.place.add.showForm')}}"><i class="fas fa-plus"></i>&nbsp;Add Place</a></li>
+          <li><a href="{{route('admin.places.deleted')}}"><i class="fas fa-times"></i>&nbsp;Deleted Places</a></li>
         </ul>
-      </li> --}}
+      </li>
+      <li class="treeview">
+        <a href="#">
+          <i class="fas fa-users"></i>&nbsp;<span>Users</span>
+          <span class="pull-right-container">
+            <i class="fa fa-angle-left pull-right"></i>
+          </span>
+        </a>
+        <ul class="treeview-menu">
+          <li><a href="{{route('admin.users')}}"><i class="fas fa-user-friends"></i>&nbsp;All Users</a></li>
+          <li><a href="{{route('admin.user.add.showForm')}}"><i class="fas fa-user-plus"></i>&nbsp;Add User</a></li>
+          <li><a href="{{route('admin.users.deleted')}}"><i class="fas fa-user-times"></i>&nbsp;Deleted Users</a></li>
+        </ul>
+      </li>
   </section>
   <!-- /.sidebar -->
 </aside>
