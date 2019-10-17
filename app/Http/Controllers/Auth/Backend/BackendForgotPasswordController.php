1a1a8550f046fe8a0d9e2ef57e256c642792000c
James McHugh <james.mchugh1988@gmail.com>
2019-10-17 12:14:48 +0100

    completed forgot password controller

--- app/Http/Controllers/Auth/Backend/BackendForgotPasswordController.php
+++ app/Http/Controllers/Auth/Backend/BackendForgotPasswordController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Auth\Backend;
 
+use App\AdminUsers;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class BackendForgotPasswordController extends Controller
 {
@@ -34,10 +36,41 @@ class BackendForgotPasswordController extends Controller
     //Overrides
 
     public function sendResetLinkEmail(Request $request){
+      $messages = [
+        'user_email.required' => "Please fill in your email address"
+      ];
 
+      $validatedData = $request->validate([
+        'user_email' => 'required'
+      ], $messages);
+
+      $data = $request->all();
+
+      $user = AdminUsers::where("user_email", $data['user_email'])->first();
+
+      if($user->count() > 0){
+        $user->user_password_reset_token=sha1(uniqid($data['user_email'], true) . $user->user_surname . strtotime("now"));;
+        $user->save();
+
+        \Mail::to($user->user_email)->send(new \App\Mail\Admin\sendEmails($user, "backend.emails.resetPassword", "Creative Insight Developer Test Admin: Reset Your Password"));
+
+        $data = array(
+          "messages" => array(
+            "success" => array("An email has been sent to you to reset, your password, please click on the link that is provided"),
+            "errors" => array()
+          ),
+        );
+
+        Auth::logout();
+        return redirect()->route('admin.login')->with($data);
+      }
     }
 
     public function showLinkRequestForm(){
+      $data = array(
+        "pageTitle" => "Request Password Reset"
+      );
 
+      return view("backend.pages.auth.passwords.email")->with($data);
     }
 }
