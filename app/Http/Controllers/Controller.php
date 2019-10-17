eb3d8e35aab889e06adc363d87134258666589d7
James McHugh <james.mchugh1988@gmail.com>
2019-10-17 09:37:54 +0100

    removed unused function and removed the guard from user function call

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -19,30 +19,6 @@ class Controller extends BaseController{
     }
 
     protected static function currentUserDetails(){
-      return Auth::guard('admin')->user()->toArray();
-    }
-
-    protected static function userIsAllowed($useridhash = null, $userLevel = 7){
-      //if user id is not set user the current user
-      if(isset($useridhash) && !empty($useridhash)){
-        $userid = Auth::guard('admin')->user()->user_id_hash;
-      }
-
-      //if user level is less than 1 then set it to 8 - which is the basic user - this is a fallback for incorrect values
-      if(isset($userLevel) && !empty($userLevel) && $userid <= 0){
-        $userLevel = 8;
-      }
-
-      $user = AdminUsers::where("user_id_hash", $useridhash)->first();
-
-      if(isset($user) && !empty($user) && $user->count() > 0){
-        if(isset($user->user_level) && !empty($user->user_level) && $user->user_level <= $userLevel){
-          return true;
-        }else{
-          return false;
-        }
-      }else{
-        return false;
-      }
+      return Auth::user()->toArray();
     }
 }
