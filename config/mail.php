cce3385f3cb5d312c6962957008395745106b61b
James McHugh <james.mchugh1988@gmail.com>
2019-10-17 12:14:03 +0100

    ammended the from address and name in config

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'james.mchugh.webdeveloper@gmail.com'),
+        'name' => env('MAIL_FROM_NAME', 'Creative Insight Developer Test'),
     ],
 
     /*
